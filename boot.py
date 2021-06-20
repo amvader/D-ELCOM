62f4afc9f0c47717e00979d6716aa6663d456116
Amvader <stevenwschmidt@gmail.com>
2021-06-20 11:50:54 -0500

    1150 062021

--- boot.py
+++ boot.py
@@ -1,12 +1,8 @@
-print("boot",end='')
+# boot.py -- run on boot-up
 import time
-import pycom
 import machine
-from _pybytes import Pybytes
-from _pybytes_config import PybytesConfig
 
 # boot.py -- run on boot-up
-print("ing",end='')
 for j in range(0,2):
     pycom.rgbled(0x00000f)
     time.sleep(.05)
@@ -32,14 +28,3 @@ for j in range(0,2):
     time.sleep(.05)
     pycom.rgbled(0x000000)
 
-print("...")
-
-print("Setup pybytes...",end='')
-pycom.nvs_set('pybytes_debug', 99)
-
-conf = PybytesConfig().read_config()
-pybytes = Pybytes(conf)
-#pybytes.start()
-print('done')
-print("Pybytes connected=",end='')
-print(pybytes.isconnected())
